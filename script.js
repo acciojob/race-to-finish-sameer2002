523a8f2bfa743ee67dc81e84565a596ac9a6d2b7
Sameer Sagar <60567209+sameer2002@users.noreply.github.com>
2023-09-02 16:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const outputDiv = document.getElementById('output');
+
+const getRandomTime = () => Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+
+window.promises = [
+    new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 3 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 4 resolved"), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve("Promise 5 resolved"), getRandomTime()))
+];
+
+Promise.any(promises)
+    .then(result => {
+        outputDiv.textContent = result;
+    })
+    .catch(error => {
+        console.error('All promises rejected:', error);
+    });
